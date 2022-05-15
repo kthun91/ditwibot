12cafd6624c5d26862888206d855ed8ffd648efd
Kevin Thunert <kevin.thunert@live.de>
2022-05-15 02:35:59 +0200

    added initial solution

---
+++ userID.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# python userID.py USERNAME CLIENT_ID OAUTH_TOKEN
+import sys
+import requests
+
+URL = 'https://api.twitch.tv/helix/users?login='+sys.argv[1]
+
+try:
+    headers = {'Client-Id': sys.argv[2], 'Authorization': 'Bearer '+sys.argv[3]}
+    req = requests.get(URL, headers=headers, timeout=15)
+    req.raise_for_status()
+    data = req.json()
+    print(data['data'][0]['id'])
+except requests.exceptions.ConnectionError as cone:
+        print("Connection Error:", cone)
+except requests.exceptions.Timeout as et:
+        print("Timeout Error:", et)
+except requests.exceptions.RequestException as re:
+        print("Error message:", re)
